61de4d4793c253b1ba0969044afeb11e8ec4122a
Masa <ktmrmshk@gmail.com>
2014-05-04 21:56:50 +0900

    bug fix and make nabi working

--- news/views.py
+++ news/views.py
@@ -20,12 +20,13 @@ def test_search(request):
         #page starts from 1 not from 0
         page = int( request.GET.get('page', '1') )
         
-        query={}
-        query['q'] = q
+#         query={}
+#         query['q'] = q
         q_for_navi = 'http://' + request.get_host()
         q_for_navi += request.path
         q_for_navi += '?'
-        q_for_navi += urllib.urlencode(query)
+        #q_for_navi += urllib.urlencode(query)
+        q_for_navi += 'q=%s' % q
         
         start = (page-1) * 20
         #DEBUG
@@ -35,6 +36,44 @@ def test_search(request):
         pub.setparam(count=20, start=start)
         pub.get()
         publist = pub.parse_dat_all()
-        return render(request, 'tmp_search-result.html', {'cnt_ret': pub.cnt_ret, 'publist': publist, 'page': page, 'q_for_navi': q_for_navi })
+        lastpage=(pub.cnt_ret-1)/20 + 1
+        pagelist=[]
+        next='t'
+        before='t'
+        if page == 1:
+            before='f'
+            if (page+10) < lastpage:
+                pagelist=[1, 2, page+10, lastpage]
+            elif (page+2) < lastpage:
+                pagelist={1, 2, 3, lastpage}
+            else:
+                pagelist=[1, 2, lastpage]
+        elif page == 2:
+            if (page+10) < lastpage:
+                pagelist=[1, 2, 3, lastpage]
+            elif (page+2) < lastpage:
+                pagelist=[1, 2, 3, lastpage]
+            else:
+                pagelist=[1, 2, lastpage]
+        elif page == lastpage:
+            next='f'
+            if 1 < (lastpage-1):
+                pagelist = [1, lastpage-1, lastpage]
+            else:
+                pagelist = [1, lastpage]
+        
+        
+        else:
+            if (page+10) < lastpage:
+                pagelist=[1, page, page+10, lastpage]
+            elif (page+1) < lastpage:
+                pagelist=[1, page, page+1, lastpage]
+            else:
+                pagelist=[1, page-1, page, lastpage]
+        
+        print pagelist
+        return render(request, 'tmp_search-result.html',
+                       {'cnt_ret': pub.cnt_ret, 'publist': publist, 'page': page, 'q_for_navi': q_for_navi,
+                        'pagelist' : pagelist, 'next':next, 'before':before })
 
     return render(request, 'tmp_search-result.html')
