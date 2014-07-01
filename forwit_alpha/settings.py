8da9fc7e958d380e14e72b42a5d6c04ab027c718
Masa <ktmrmshk@gmail.com>
2014-07-01 20:50:27 +0900

    added setting.py

---
+++ forwit_alpha/settings.py
@@ -0,0 +1,136 @@
+"""
+Django settings for forwit_alpha project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'wva*8v4ei#j_1lrz(az(@l)r7%yz(rzdo(lfv&6)05#h$pbylo'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ["localhost"]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'news',
+    'south',
+    'social_auth',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'forwit_alpha.urls'
+
+WSGI_APPLICATION = 'forwit_alpha.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+#STATIC_URL = '/static/'
+STATIC_URL = '/html_design/'
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, 'html_design'),
+)
+
+TEMPLATE_DIRS = (
+                 'html_design',
+                 'news/templates',
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.twitter.TwitterBackend',
+    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuthBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'social_auth.backends.google.GoogleBackend',
+    'social_auth.backends.yahoo.YahooBackend',
+    'social_auth.backends.browserid.BrowserIDBackend',
+    'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.contrib.disqus.DisqusBackend',
+    'social_auth.backends.contrib.livejournal.LiveJournalBackend',
+    'social_auth.backends.contrib.orkut.OrkutBackend',
+    'social_auth.backends.contrib.foursquare.FoursquareBackend',
+    'social_auth.backends.contrib.github.GithubBackend',
+    'social_auth.backends.contrib.vk.VKOAuth2Backend',
+    'social_auth.backends.contrib.live.LiveBackend',
+    'social_auth.backends.contrib.skyrock.SkyrockBackend',
+    'social_auth.backends.contrib.yahoo.YahooOAuthBackend',
+    'social_auth.backends.contrib.readability.ReadabilityBackend',
+    'social_auth.backends.contrib.fedora.FedoraBackend',
+    'social_auth.backends.OpenIDBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+TWITTER_CONSUMER_KEY         = 'XaIeH639tKyIj1mUmMuviWPIE'
+TWITTER_CONSUMER_SECRET      = 'gmZOdkqSecpuftKYS1RJodC2LNg93JCXTsgpX7RbSAelrQ1Uph'
+FACEBOOK_APP_ID='778794818797349'
+FACEBOOK_API_SECRET='f32010e1b10ba1e7b3a39ac83ccef2cd'
+GOOGLE_OAUTH2_CLIENT_ID = '426736972927-hesa75rqvvi58v5h5hecu6t16igddcgj.apps.googleusercontent.com'
+GOOGLE_OAUTH2_CLIENT_SECRET = 'wLk9oMpf8LvIfBiMqymDv6JD'
+
+SESSION_SERIALIZER='django.contrib.sessions.serializers.PickleSerializer'
+
+
+
+LOGIN_REDIRECT_URL = '/loggedin/'
+SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/new_socialuser/'
